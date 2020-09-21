30f46d642a34ae76c524c196cc302ca67448be2a
fabianzboni <fabianzapata2001@hotmail.com>
2020-09-20 20:51:16 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Proyecto-poo
+# proyecto-poo
  
--- src/main/java/model/Artista.java
+++ src/main/java/model/Artista.java
@@ -1,5 +1,12 @@
 package model;
 
 public class Artista {
+	private long codigoArtista;
+	private long cedula;
+	private String nombre;
+	private String apellido;
+	private Calendar fechaNacimiento;
+	private long telefono;
+	
 
 }
